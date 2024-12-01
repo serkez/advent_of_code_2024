cf3cca6bb54a52b48ae2398ce6958a3d711a4f95
Naomi S <naomiserkez@gmail.com>
2024-12-01 11:40:17 +0200

    Initial commit

---
+++ day1_HistorianHysteria.py
@@ -0,0 +1,7 @@
+# goal : take n lines of input, for each line, get 2 numbers split on " "
+# create 1 list with all the numbers on the right, and one for the left
+# sort the lists
+# calculate the difference between the values in both lists for all i
+# add that value to a variable
+# print the variable
+# point, finding the difference between the smallest number in each list for each smallest numebr
